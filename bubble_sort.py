6f5126a5f2f9fd85064f6acdee0806063e25a208
Shinpei Aoki <shi.aoki@opt.ne.jp>
2021-01-16 19:24:47 +0900

    バブルソートの計測を追加
    bubble

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,17 +1,26 @@
 from util.input_generator import generate_random_list
+from util.stop_watch import stop_watch
 
-if __name__ == "__main__":
-    target_list = generate_random_list(1000)
 
-    for i in range(len(target_list)):
+@stop_watch
+def sort(input_list: list) -> list:
+    """入力されたリストをソートする"""
+    work_list = input_list.copy()
+    for i in range(len(work_list)):
         if i == 0:
             continue
         else:
             for j in reversed(range(0, i)):
-                if target_list[i] < target_list[j]:
-                    target_list[i], target_list[j] = target_list[j], target_list[i]
+                if work_list[i] < work_list[j]:
+                    work_list[i], work_list[j] = work_list[j], work_list[i]
                     i = j
                 else:
                     break
+    return work_list
 
-    print(f"Result list: {target_list}")
+
+if __name__ == "__main__":
+    target_list = generate_random_list(1000)
+    result_list = sort(target_list)
+
+    print(f"Result list: {result_list}")
